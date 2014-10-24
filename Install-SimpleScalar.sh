4c0d2684047ccd8e4fe0cba2de336ad4a469cde4
Simon DENEL <simondenel1@gmail.com>
2014-10-24 14:55:14 +0200

    Import from http://simon.denel.fr

---
+++ Install-SimpleScalar.sh
@@ -0,0 +1,136 @@
+#!/bin/sh
+# This script can be launch using the command-line "wget http://simon.denel.fr/Articles/How-To-Install-SimpleScalar/Install-SimpleScalar.sh && chmod +x Install-SimpleScalar.sh && ./Install-SimpleScalar.sh"
+
+# This is the only line requiring root permission
+sudo apt-get install bison flex gzip gcc-multilib ia32-libs zenity xdg-utils
+
+# Will install SimpleScalar in ~/SimpleScalar
+export IDIR=$HOME"/SimpleScalar"
+
+mkdir $IDIR
+cd $IDIR
+
+# A simple wget commandline would not comply with licences, so SimpleScalar has to be downloaded manually
+TEXT="Thanks to:\n-Accept the licence then download the archive file\n-Place the archive here: "$IDIR"\n-Close your web-browser"
+xdg-open http://www.simplescalar.com/agreement.php3?simplesim-3v0e.tgz &
+echo -e $TEXT
+sleep 0.5
+zenity --info --text "$TEXT" &
+read -p "Please press ENTREE once the task is done" a
+
+wget http://www.simplescalar.com/downloads/simpletools-2v0.tgz
+wget http://www.simplescalar.com/downloads/simpleutils-2v0.tgz
+
+gunzip  *.tgz
+tar -xf simpletools-*.tar
+tar -xf simpleutils-*.tar
+tar -xf simplesim-*.tar
+rm *.tar
+rm *.tgz
+
+### binutils Compilation ###
+cd binutils-*
+./configure --host=i386-pc-linux --target=sslittle-na-sstrix --with-gnu-as --with-gnu-ld --prefix=$IDIR
+
+# Avoiding:
+# vasprintf.c:48:3: error: invalid initializer
+
+sed -i -e "s/va_list ap = args;/va_list ap; va_copy(ap, args);/g" libiberty/vasprintf.c
+# Avoiding:
+# vasprintf.c:35:7: erreur: conflicting types for ‘malloc’
+sed -i -e "s/char \*malloc ();/\/\/char \*malloc ();/g" libiberty/vasprintf.c
+
+# Avoiding:
+# ./ldlex.l:477:7: error: 'yy_current_buffer' undeclared (first use in this function)
+sed -i -e "s/yy_current_buffer/YY_CURRENT_BUFFER/g" ld/ldlex.l
+
+# avoiding errors like: /usr/lib/gcc/x86_64-linux-gnu/4.6/include/varargs.h:4:2: error: #error "GCC no longer implements <varargs.h>."
+# but also: ldmisc.c:318:31: error: expected expression before ‘char’
+sed -i -e "s/varargs.h/stdarg.h/g" ld/ldmisc.c
+
+# Avoiding errors due to an outdated code
+# Inspired from http://zealoct.wordpress.com/2011/04/19/install-simplescalar-on-ubuntu-10-10-with-gcc-4-4/
+# avoiding: ldmisc.c:348:15: error: macro "va_start" requires 2 arguments, but only 1 given
+sed -i -e "s/  va_list arg;/\/\/  va_list arg;/g" ld/ldmisc.c
+sed -i -e "s/  va_start/\/\/  va_start/g" ld/ldmisc.c
+sed -i -e "s/  file/\/\/  file/g" ld/ldmisc.c
+sed -i -e "s/  fmt/\/\/  fmt/g" ld/ldmisc.c
+sed -i -e "s/  vfinfo/\/\/  vfinfo/g" ld/ldmisc.c
+sed -i -e "s/  va_end/\/\/  va_end/g" ld/ldmisc.c
+
+# Avoiding:
+# ldmisc.c:402:6: error: expected ‘=’, ‘,’, ‘;’, ‘asm’ or ‘__attribute__’ before ‘va_dcl’
+# ldmisc.c:344:6: error: expected declaration specifiers before ‘va_dcl’
+sed -i -e "s/(va_alist)/()/g" ld/ldmisc.c
+sed -i -e "s/     va_dcl/\/\/     va_dcl/g" ld/ldmisc.c
+sed -i -e "s/     FILE \*fp;/\/\/     FILE \*fp;/g" ld/ldmisc.c
+sed -i -e "s/     char \*fmt;/\/\/     char \*fmt;/g" ld/ldmisc.c
+sed -i -e "s/vfinfo(fp, fmt, arg)/vfinfo(FILE \*fp, char \*fmt, va_list arg)/g" ld/ldmisc.c
+
+make all
+make install
+
+
+cd ../simplesim*
+make config-pisa
+make
+# You can check that SimpleScalar (not the toolchain) works with the command-line:
+# ./sim-safe tests-alpha/bin/test-math
+
+
+cd ../
+cd gcc-*
+
+./configure --host=i386-pc-linux --target=sslittle-na-sstrix --with-gnu-as --with-gnu-ld --prefix=$IDIR
+
+# Avoiding:
+# insn-output.c:676:5: error: stray ‘\’ in program
+sed -i 's/return \\"FIXME\\\\n/return \\"FIXME\\\\n\\\\/g' config/ss/ss.md
+#sed -i 's/return \"FIXME\\n/return \"FIXME\\n\\/g' insn-output.c
+
+# Do not include LIBGCC2_INCLUDES leads to
+# ./libgcc2.c:1384: stdio.h: No such file or directory
+# make: *** [libgcc2.a] Error 1
+# make has to be launched before to correct errors...
+
+make LANGUAGES="c c++" CFLAGS="-O3" CC="gcc"
+
+# Avoiding:
+# decl.c:3605:3: error: lvalue required as increment operand
+sed -i -e "s/\*((void \*\*)__o->next_free)++ = ((void \*)datum);/\*((void \*\*)__o->next_free++) = ((void \*)datum);/g" obstack.h
+
+# Avoiding:
+# cxxmain.c:2978: error: conflicting types for ‘malloc’ - uniquement pour gcc 2.7
+
+#chmod 755 cxxmain.c
+#sed -i -e "s/char \* malloc ();/\/\/char \* malloc ();/g" cxxmain.c
+#sed -i -e "s/char \* realloc ();/\/\/char \* realloc ();/g" cxxmain.c
+
+# Avoiding:
+# sdbout.c:57:18: fatal error: syms.h: No such file or directory
+sed -i -e "s/#include <syms.h>/#include \"gsyms.h\"/g" sdbout.c
+
+# Avoiding:
+# cccp.c:194:14: error: conflicting types for ‘sys_errlist’
+sed -i -e "s/extern char \*sys_errlist\[\];/\/\/extern char \*sys_errlist\[\];/g" cccp.c
+# Avoiding:
+# ./cp/g++.c:90:14: error: conflicting types for ‘sys_errlist’
+sed -i -e "s/extern char \*sys_errlist\[\];/\/\/extern char \*sys_errlist\[\];/g" cp/g++.c
+# Avoiding: 
+# gcc.c:172:14: erreur: conflicting types for ‘sys_errlist’
+sed -i -e "s/extern char \*sys_errlist\[\];/\/\/extern char \*sys_errlist\[\];/g" gcc.c
+
+make LANGUAGES="c c++" CFLAGS="-O3" CC="gcc"
+# If you do not uncompress simpletools at the right place, You will face:
+# /usr/include/_G_config.h:53: unknown machine mode `_G_int16_t'
+# /usr/include/_G_config.h:54: unknown machine mode `_G_int32_t'
+# /usr/include/_G_config.h:55: unknown machine mode `_G_uint16_t'
+# /usr/include/_G_config.h:56: unknown machine mode `_G_uint32_t'
+make install LANGUAGES="c c++" CFLAGS="-O3" CC="gcc"
+
+echo 'PATH='$IDIR'/bin:$PATH' >> ~/.bashrc
+cd ../simplesim-*
+echo 'PATH='$PWD':$PATH' >> ~/.bashrc
+
+echo "This is it! Please restart your session in order to update your global variables."
+
---
+++ Readme.md
@@ -0,0 +1,23 @@
+#How-to install SimpleScalar on Ubuntu#
+[SimpleScalar](http://www.simplescalar.com/) is a VLIW processor simulator. It permits to access measures like execution time, caches misses, and branch mis-prediction rate. It is entirely scalable: cache size and type, number of UAL entities, branch predictor type are customizable. It may be used as a tool for understanding Design-Space-Exploration. Unfortunately, the installation is quite complex due to an outdated code source. Thank you to Kang Li for his fix!
+
+The following script intends to automate the installation process with Ubuntu. It has been tested with Ubuntu 12.04 and gcc 4.6. It may not resolve all the errors you will have to face: don't hesitate to contact me (see my resume) if you think the script has to be improved. If you can choose not to use this simulator, you may want to have a glance at other simulators like [SoCLib](http://www.soclib.fr/) or [gem5](http://www.m5sim.org/). A more exhaustive list is available at [wikipedia (en)](http://en.wikipedia.org/wiki/Computer_architecture_simulator#Implementations)
+## The script ##
+Install-SimpleScalar.sh will create ~/SimpleScalar, and install SimpleScalar in it.
+Set the script as executable, run it, this it it! You may be able to compile your first program using a command-line such as: "`sslittle-na-sstrix-gcc -x c++ main.c`"
+## Miscellaneous ##
+
+- gcc 2.6 does not accept inline comments with C. A simple workaround is to compile your C-code as if it was a C++ code: use for instance `sslittle-na-sstrix-gcc -x c++ main.c`
+- You may want to change some compilation flags in the SimpleScalar MakeFile: Replace "`-O0`" by "`-O3 -march=native -fstrict-aliasing`" and removing the "`-DDEBUG`" flag gives a substantial speed gain (near x2.0)
+- Dimensioning your architecture is based on a compromise: Cache speed decrease with size. For a quantitative idea of the speed you can expect from a [cache](http://en.wikipedia.org/wiki/Cache_%28computing%29), you may be interested by the tool [CACTI](http://www.hpl.hp.com/research/cacti/), luckily far more simple to install than SimpleScalar.
+
+## Connected links ##
+
+Are you stuck with a bug not described here? You may find the solution in the following guides:
+
+- [Simple Scalar Installation Tips](http://www.cse.iitd.ernet.in/~drajeswari/ss_installn.html)
+- [Install SimpleScalar on Ubuntu 10.10 with Gcc-4.4](http://zealoct.wordpress.com/2011/04/19/install-simplescalar-on-ubuntu-10-10-with-gcc-4-4/)
+- [McAiT (Rev 1.0) User Manual](http://www.neu-rtes.org/mcait/McAiT_UM_1.0.pdf)
+- [Installing SimpleScalar: common problems and their solutions](http://www.neu-rtes.org/mcait/simplescalar_install_notes.pdf)
+
+
